2164ff807a83ba30d11f3d676610ed2decf0b197
Njinga Nkosi <47808032+Njinga@users.noreply.github.com>
2020-05-28 03:59:56 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <h4 class="class="bg-primary text-white text-center p-2">HELLO WORLD</h4>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
